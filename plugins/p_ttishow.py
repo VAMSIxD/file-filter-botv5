9bae5c19a7e2eb0f13fc58b8605d25e3c08c8de8
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2023-08-26 17:06:56 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -40,11 +40,11 @@ async def save_group(bot, message):
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', url=f"https://t.me/{temp.U_NAME}?start=help"),
             InlineKeyboardButton('💎 Updates 💎', url='https://t.me/m_house786')
             ],[
-            InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/malik_bots')
+            InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/PVTSERVER')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me Contact Owner >> @sahid_malik.</b>",
+            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me Contact Owner >> @DEXTERGRAY.</b>",
             reply_markup=reply_markup)
     else:
         settings = await get_settings(message.chat.id)
